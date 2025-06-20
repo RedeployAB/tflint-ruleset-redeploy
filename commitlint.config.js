42c2548b80966d61b4f3c8fe486e6cc849343870
Lars Åkerlund <42139799+larsakerlund@users.noreply.github.com>
2025-06-20 23:02:30 +0200

    chore: enforce custom commit types

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,27 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
   helpUrl: "https://www.conventionalcommits.org/",
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "build",
+        "ci",
+        "docs",
+        "feat",
+        "fix",
+        "perf",
+        "refactor",
+        "style",
+        "test",
+        "environment",
+        "infra",
+        "chore",
+        "deps",
+      ],
+    ],
+  },
   // We need this until https://github.com/dependabot/dependabot-core/issues/2445
   // is resolved.
   ignores: [(msg) => /Signed-off-by: dependabot\[bot]/m.test(msg)],
